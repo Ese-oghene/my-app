7be9ef10a5cc0ed3fd845656b763d34962c61416
Ese-oghene <oghenee08@gmail.com>
2025-08-26 21:54:22 +0100

    fixed: error

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,7 @@ import HeroSection from '../components/HeroSection'
 import About from '../components/About'
 import Feature from '../components/Feature'
 import HowWorks from '../components/HowWorks'
-import testimonies from '../components/testimonials'
+// import testimonies from '../components/testimonials'
 
 const Home = () => {
   return (
